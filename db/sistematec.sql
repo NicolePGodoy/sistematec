28af488a15c7229912d70c5b2863f2dd620ea0f4
NicolePGodoy <nicolepgodoy29@gmail.com>
2024-06-19 21:23:18 -0500

    DB actualizada

--- db/sistematec.sql
+++ db/sistematec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 16, 2024 at 05:01 AM
+-- Generation Time: Jun 20, 2024 at 02:22 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -41,12 +41,49 @@ CREATE TABLE `calendar` (
 --
 
 INSERT INTO `calendar` (`id`, `date`, `hour`, `customer_id`, `barber_id`, `service_id`) VALUES
-(1, '2024-05-24', '03:00:00', 1, 2, 1),
-(2, '2024-05-31', '04:45:00', 1, 7, 2),
-(3, '2024-05-18', '05:15:00', 1, 4, 2),
-(4, '2024-05-10', '12:00:00', 1, 5, 3),
-(5, '2024-05-10', '05:21:00', 1, 4, 2),
-(6, '2024-05-17', '03:46:00', 7, 7, 2);
+(7, '2024-06-07', '22:28:00', 1, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `query` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`id`, `title`, `description`, `query`) VALUES
+(1, 'ccrt', 'asdas', 'asdsadfgvdvd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int NOT NULL,
+  `id_user` int DEFAULT NULL,
+  `rating` tinyint DEFAULT NULL,
+  `comment` text COLLATE utf8mb4_general_ci
+) ;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `id_user`, `rating`, `comment`) VALUES
+(1, 4, 0, 'asd asdas '),
+(2, 7, 5, 'Excelente Barbero'),
+(3, 7, 3, 'Bien');
 
 -- --------------------------------------------------------
 
@@ -65,10 +102,9 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `description`, `price`) VALUES
-(1, 'Corte sencillo', 16000),
-(2, 'Corte + barba', 24000),
-(3, 'Corte + lavado', 20000),
-(4, 'Corte + cejas', 22000);
+(1, 'afeitado', 8000),
+(5, 'sdfdf', 111),
+(7, 'sfsdf cuatro :)', 3434444);
 
 -- --------------------------------------------------------
 
@@ -94,9 +130,11 @@ INSERT INTO `users` (`id`, `name`, `lastname`, `correo`, `password`, `rol`) VALU
 (2, 'Nicole', 'Pedraza', 'nico@gmail.com', '$2y$10$aRPYOZW/zrYgdL4a0zotaeFqm0IUKyRA3VnDDk/70mrX8xXJfyIB6', 'admin'),
 (3, 'Dayana', 'Godoy', 'nicoe3ee@gmail.com', '$2y$10$.4YWFTyDMh3xmKdkrBzNTOspqYQC.cmgORUHcAGrmGfQcf5D1uFaS', 'user'),
 (4, 'Camill', 'ddd', 'dd@gmail.com', '$2y$10$k7PkFJAPgdOxzuEFUS2kXet4UOEDudy5QUH.caJpnAMzaikmbhEAq', 'barber'),
-(5, 'aaa', 'ccc', 'dd@mail.com', '$2y$10$.8a1Zha.jxdWvSkFbUYHdeiksNBlxr6hVGd3PgDoApuZEReErBaSm', 'barber'),
-(6, 'wew', 'werewd', 'crss@gmail.com', '$2y$10$VNRZi6UMs/mVgSLzvPaofeBpLK4WJGHz7MqlSG8RG8U.1gXPxPEx6', 'barber'),
-(7, 'Cristian', 'Pedraza', 'cp@gmail.com', '$2y$10$ZzQAs4sm3etaU463jkrR9O1elUWSFAwZa3ZpYDnVt1KVqlnBoW0DS', 'barber');
+(7, 'Cristian', 'Pedraza', 'cp@gmail.com', '$2y$10$ZzQAs4sm3etaU463jkrR9O1elUWSFAwZa3ZpYDnVt1KVqlnBoW0DS', 'barber'),
+(8, 'Nicole', 'Pedraza', 'nicolepgodoy29@gmail.com', '$2y$10$aXvkMkZN6.wOJ.npyimRLeH2.Fsp5zK5lC9oA2FcsmBMmih8NeYGG', 'barber'),
+(11, 'Cristian', '', 'cp@gmail.com', '145236', 'usuario'),
+(12, 'Dayana', 'Godoy', 'nicolepgodoy29@gmail.com', '$2y$10$iG6YfoH3DW/2r69fWPUhceKY/bBmiLHm/ea/2RukDee2Scfo9jwXO', 'admin'),
+(13, 'camilo', 'torres', 'ct@gmail.com', '$2y$10$qkC5vV/R8ijy7BJY.vtrvOovcPkgcVuZAgs5OgWKGrtGRBMSiZQj2', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -110,6 +148,19 @@ ALTER TABLE `calendar`
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `barber_id` (`barber_id`),
   ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `services`
@@ -131,19 +182,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `calendar`
 --
 ALTER TABLE `calendar`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -156,6 +219,12 @@ ALTER TABLE `calendar`
   ADD CONSTRAINT `calendar_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `calendar_ibfk_2` FOREIGN KEY (`barber_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `calendar_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
