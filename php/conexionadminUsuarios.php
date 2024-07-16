974b69d9f65766228101e629c185180a8733f3e4
NicolePGodoy <nicolepgodoy29@gmail.com>
2024-07-15 22:09:24 -0500

    Hello

--- php/conexionadminUsuarios.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "sistematec";
-
-    $conexion = new mysqli($servername, $username, $password, $dbname);
-    $conexion ->set_charset("utf8");
-    ?>
